87faeee1228ac983ac15a8589d80d073505f8a7c
Nitin Chaudhary <nc1575612@gmail.com>
2025-08-05 23:04:40 +0530

    init theme

--- App.tsx
+++ App.tsx
@@ -2,11 +2,14 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { PaperProvider } from 'react-native-paper';
 import { LightTheme } from './src/theme/light';
+import { DarkTheme } from './src/theme/dark';
 import AppNavigator from './src/navigation/AppNavigator';
+import { useColorScheme } from 'react-native';
 
 export default function App() {
+  const colorScheme = useColorScheme();
   return (
-    <PaperProvider theme={LightTheme}>
+    <PaperProvider theme={colorScheme === 'dark' ? DarkTheme : LightTheme}>
       <NavigationContainer>
         <AppNavigator />
       </NavigationContainer>
